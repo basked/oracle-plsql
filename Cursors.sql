5745768029f54f8ce0c7edde40bfa01295929f61
laravel.dev <basket.baza@gmail.com>
2022-02-03 11:48:30 +0300

    Procedure and coursors

---
+++ Cursors.sql
@@ -0,0 +1,34 @@
+﻿
+-- Курсор с параметром
+DECLARE
+  CURSOR cat(p_id IN categories.id%TYPE) IS
+    SELECT * FROM CATEGORIES c WHERE c.id > p_id;
+BEGIN
+  FOR rec IN cat(1) LOOP
+    dbms_output.put_line(rec.title);
+  END LOOP;
+END;
+-- Курсоры и возвращаемое значение из DML операции
+DECLARE
+  CURSOR cat_cur IS
+    SELECT * FROM categories;
+  cat_rec          categories%ROWTYPE;
+  "l_count_insert" PLS_INTEGER;
+  -- Вставка данных и возврат количества вставленных записей
+  PROCEDURE insert_data(categories_rec IN categories%ROWTYPE,
+                        count_insert   OUT PLS_INTEGER) IS
+  BEGIN
+    INSERT INTO categories2 VALUES categories_rec;
+    count_insert := SQL%ROWCOUNT;
+  END insert_data;
+BEGIN
+  OPEN cat_cur;
+  LOOP
+    FETCH cat_cur
+      INTO cat_rec;
+    insert_data(cat_rec, "l_count_insert");
+    dbms_output.put_line('Вставлено записей:' || "l_count_insert");
+    EXIT WHEN cat_cur%NOTFOUND;
+  END LOOP;
+  CLOSE cat_cur;
+END;
---
+++ Exceptions.sql
@@ -0,0 +1,61 @@
+﻿/*-=1=- ОПИСАНИЕ ОШИБКИ В ОДНОЙ ПРОЦЕДУРЕ*/
+DECLARE
+  em_bas1 EXCEPTION;
+  em_bas2 EXCEPTION;
+  PROCEDURE call_error1 IS
+  BEGIN
+    RAISE em_bas2;
+  EXCEPTION
+    -- через OR вызывается исключение 
+    WHEN em_bas1 OR em_bas2 THEN
+      dbms_output.put_line(Dbms_Utility.format_error_backtrace);
+  END call_error1;
+  ----------------------
+  PROCEDURE call_error1_1 IS
+  BEGIN
+    dbms_output.put_line('call_error1_1');
+  END call_error1_1;
+  PROCEDURE call_error2 IS
+  BEGIN
+    call_error1;
+    call_error1_1;
+  END call_error2;
+BEGIN
+  call_error2;
+END;
+----------------------------------------------------
+/*-=2=- СТЕК ВЫЗОВЫ ОШИБОК В РАЗНЫХ ПРОЦЕДУРАХ*/
+CREATE OR REPLACE PROCEDURE proc1 IS 
+BEGIN
+  dbms_output.put_line('Вызов процедуры proc1');
+  RAISE NO_DATA_FOUND;
+END proc1;
+
+CREATE OR REPLACE PROCEDURE proc2 IS
+BEGIN
+  dbms_output.put_line('Вызов процедуры proc2');
+  proc1;
+END proc2;
+
+CREATE OR REPLACE PROCEDURE proc3 IS
+BEGIN
+  dbms_output.put_line('Вызов процедуры proc3');
+  -- вызываем и бросаем исключение из первой процедуры
+  proc2;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    bt.show_info(Dbms_Utility.format_error_backtrace);
+    -- dbms_output.put_line(Dbms_Utility.format_error_backtrace);
+END proc3;
+
+BEGIN
+ proc3;
+END;
+----------------------------------------------------------------------
+/*-=3=- ВЫВОД ОШИБКИ АНАЛИЗИРУЯ СТЕК СМ PACKAGE BT*/
+----------------------------------------------------------------------
+/*-=4=- ОШИБКИ ПРИ КОПИЛЯЦИИ ПРОЦЕДУРЫ */
+SELECT *
+FROM user_errors
+
+ 
