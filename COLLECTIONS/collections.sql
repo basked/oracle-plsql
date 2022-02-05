084b545abf26e83a4eb1001b7836a92728f0fed7
laravel.dev <basket.baza@gmail.com>
2022-02-05 10:23:26 +0300

    Loop collection

--- COLLECTIONS/collections.sql
+++ COLLECTIONS/collections.sql
@@ -1,16 +1,32 @@
 ﻿DECLARE
-  TYPE list_of_countries_t IS TABLE OF COUNTRIES.Country_Name%TYPE INDEX BY PLS_INTEGER;
-  list_of_countries list_of_countries_t;
-  l_row             PLS_INTEGER;
+  TYPE list_of_countries_str_t IS TABLE OF COUNTRIES.Country_Name%TYPE INDEX BY VARCHAR2(6);
+  TYPE list_of_countries_int_t IS TABLE OF COUNTRIES.Country_Name%TYPE INDEX BY PLS_INTEGER;
+  list_of_countries_str list_of_countries_str_t;
+  list_of_countries_int list_of_countries_int_t;
+  l_row_str             VARCHAR(6);
+  l_row_int             PLS_INTEGER;
 BEGIN
-  list_of_countries(0) := 'a';
-  list_of_countries(1) := 'b';
-  list_of_countries(2) := 'c';
-  list_of_countries(3) := 'd';
-  l_row := list_of_countries.FIRST;
-  WHILE (l_row IS NOT NULL) LOOP
-    dbms_output.put_line(list_of_countries(l_row));
-    l_row := list_of_countries.next(l_row);
+  -- Работа с ассативным масивом - индексы - это строки
+  list_of_countries_str('0') := 'a';
+  list_of_countries_str('1') := 'b';
+  list_of_countries_str('2') := 'c';
+  list_of_countries_str('3') := 'd';
+  list_of_countries_str('bas') := 'e';
+  dbms_output.put_line('Обращение к индексу по строке');
+  l_row_str := list_of_countries_str.FIRST;
+  WHILE (l_row_str IS NOT NULL) LOOP
+    dbms_output.put_line(list_of_countries_str(l_row_str));
+    l_row_str := list_of_countries_str.next(l_row_str);
   END LOOP;
-
+  -- Работа с ассативным масивом - индексы - это числа
+  list_of_countries_int(1) := 'a';
+  list_of_countries_int(2) := 'b';
+  list_of_countries_int(3) := 'c';
+  list_of_countries_int(4) := 'd';
+  dbms_output.put_line('Обращение к индексу по номеру');
+  l_row_int := list_of_countries_int.FIRST;
+  WHILE (l_row_int IS NOT NULL) LOOP
+    dbms_output.put_line(list_of_countries_int(l_row_int));
+    l_row_int := list_of_countries_int.next(l_row_int);
+  END LOOP;
 END;
---
+++ COLLECTIONS/loop_collections.sql
@@ -0,0 +1,27 @@
+﻿-- ПЕРЕБОР ВСЕХ ЭЛЕМЕНТОВ КОЛЕКЦИИ КОЛЛЕКЦИЙ
+
+DECLARE
+  TYPE t_aa IS TABLE OF VARCHAR2(10) INDEX BY PLS_INTEGER;
+  TYPE t_aa2 IS TABLE OF t_aa INDEX BY PLS_INTEGER;
+  t  t_aa;
+  t2 t_aa2;
+  l_row  PLS_INTEGER;
+  l_row2 PLS_INTEGER;
+BEGIN
+  t(0) := '0';
+  t(1) := '1';
+  t(2) := '2';
+  t2(0) := t;
+  t(3) := '3';
+  t2(1) := t;
+  l_row2 := t2.first;
+  WHILE (l_row2 IS NOT NULL) LOOP
+    l_row := t2(l_row2).first;
+    WHILE (l_row IS NOT NULL) LOOP
+      dbms_output.put_line('t(' || l_row2 || ')(' || l_row || ')=' ||
+                           t2(l_row2) (l_row));
+      l_row := t2(l_row2).next(l_row);
+    END LOOP;
+    l_row2 := t2.next(l_row2);
+  END LOOP;
+END;
