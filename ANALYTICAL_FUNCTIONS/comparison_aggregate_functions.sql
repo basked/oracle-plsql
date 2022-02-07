6adecd153acd3316f9001069c0574ee5fc03b2a6
laravel.dev <basket.baza@gmail.com>
2022-02-07 20:17:40 +0300

    Analytic functions

---
+++ ANALYTICAL_FUNCTIONS/comparison_aggregate_functions.sql
@@ -0,0 +1,66 @@
+﻿-- Без GROUP BY 
+SELECT d.department_name, e_out.salary AS max_salary
+  FROM departments d
+  LEFT JOIN employees e_out
+    ON d.department_id = e_out.department_id
+ WHERE e_out.salary IN (SELECT MAX(salary) FROM employees e_in WHERE e_in.department_id=e_out.department_id)
+ORDER BY d.department_name
+
+/*
+ Plan Hash Value  : 1152507038 
+
+-------------------------------------------------------------------------------------------
+| Id  | Operation                         | Name        | Rows | Bytes  | Cost | Time     |
+-------------------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT                  |             | 3267 | 153549 |   10 | 00:00:01 |
+| * 1 |   FILTER                          |             |      |        |      |          |
+|   2 |    SORT GROUP BY                  |             | 3267 | 153549 |   10 | 00:00:01 |
+| * 3 |     HASH JOIN                     |             | 3267 | 153549 |    9 | 00:00:01 |
+|   4 |      MERGE JOIN                   |             |  106 |   4240 |    6 | 00:00:01 |
+|   5 |       TABLE ACCESS BY INDEX ROWID | DEPARTMENTS |   27 |    567 |    2 | 00:00:01 |
+|   6 |        INDEX FULL SCAN            | DEPT_ID_PK  |   27 |        |    1 | 00:00:01 |
+| * 7 |       SORT JOIN                   |             |  107 |   2033 |    4 | 00:00:01 |
+|   8 |        TABLE ACCESS FULL          | EMPLOYEES   |  107 |   2033 |    3 | 00:00:01 |
+|   9 |      TABLE ACCESS FULL            | EMPLOYEES   |  107 |    749 |    3 | 00:00:01 |
+-------------------------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+------------------------------------------
+* 1 - filter("E_OUT"."SALARY"=MAX("SALARY"))
+* 3 - access("E_IN"."DEPARTMENT_ID"="E_OUT"."DEPARTMENT_ID")
+* 7 - access("D"."DEPARTMENT_ID"="E_OUT"."DEPARTMENT_ID")
+* 7 - filter("D"."DEPARTMENT_ID"="E_OUT"."DEPARTMENT_ID")
+
+*/
+--с учётом GROUP BY 
+SELECT d.department_name,  MAX(e.salary)  AS max_salary
+  FROM departments d
+  LEFT JOIN employees e
+    ON d.department_id = e.department_id
+    WHERE e.department_id IS NOT NULL
+ GROUP BY d.department_name
+ORDER BY d.department_name
+
+/*
+Plan Hash Value  : 374846896 
+
+----------------------------------------------------------------------------------------
+| Id  | Operation                       | Name        | Rows | Bytes | Cost | Time     |
+----------------------------------------------------------------------------------------
+|   0 | SELECT STATEMENT                |             |    4 |    92 |    7 | 00:00:01 |
+|   1 |   SORT GROUP BY                 |             |    4 |    92 |    7 | 00:00:01 |
+|   2 |    MERGE JOIN                   |             |  105 |  2415 |    6 | 00:00:01 |
+|   3 |     TABLE ACCESS BY INDEX ROWID | DEPARTMENTS |   27 |   432 |    2 | 00:00:01 |
+|   4 |      INDEX FULL SCAN            | DEPT_ID_PK  |   27 |       |    1 | 00:00:01 |
+| * 5 |     SORT JOIN                   |             |  106 |   742 |    4 | 00:00:01 |
+| * 6 |      TABLE ACCESS FULL          | EMPLOYEES   |  106 |   742 |    3 | 00:00:01 |
+----------------------------------------------------------------------------------------
+
+Predicate Information (identified by operation id):
+------------------------------------------
+* 5 - access("D"."DEPARTMENT_ID"="E"."DEPARTMENT_ID")
+* 5 - filter("D"."DEPARTMENT_ID"="E"."DEPARTMENT_ID")
+* 6 - filter("E"."DEPARTMENT_ID" IS NOT NULL)
+*/
+
+
--- COLLECTIONS/collection_pkg.sql
+++ COLLECTIONS/collection_pkg.sql
@@ -4,19 +4,46 @@
   TYPE employee_aat IS TABLE OF employee_cur%ROWTYPE INDEX BY PLS_INTEGER;
   employee_rec employee_cur%ROWTYPE;
   employee_c   employee_aat;
-  PROCEDURE output_employees;
   PROCEDURE init_data;
+  PROCEDURE output_employees;
+  PROCEDURE change_park(p_title         IN auto_park.title%TYPE,
+                        p_auto_park_rec OUT auto_park%ROWTYPE);
 END bas_col;
-
+/
 CREATE OR REPLACE PACKAGE BODY bas_col IS
   PROCEDURE init_data IS
   BEGIN
     dbms_output.put_line('Data was initialize!');
   END;
+  --
+  PROCEDURE change_park(p_title IN auto_park.title%TYPE,
+                        p_auto_park_rec OUT auto_park%ROWTYPE) IS
+    collect_c auto_park.names_auto%TYPE;
+  BEGIN
+    BEGIN
+      SELECT names_auto
+        INTO collect_c
+        FROM auto_park
+       WHERE title = p_title;
+      collect_c.trim(2);
+      collect_c.extend;
+      collect_c(collect_c.last) := 'new item collect11';
+      collect_c.extend;
+      collect_c(collect_c.last) := 'new item collect21';
+    EXCEPTION
+      WHEN OTHERS THEN
+        dbms_output.put_line('LIMIT COLLECTION = ' || collect_c.limit);
+    END;
+    UPDATE auto_park
+       SET title = 'AUTOPARK', names_auto = collect_c
+     WHERE title = p_title
+    RETURNING title, names_auto INTO p_auto_park_rec;
+  END;
+
   -- 
   PROCEDURE output_employees IS
   BEGIN
-   -- WORK WHITH RECORDS
+    -- WORK WHITH RECORDS
     sf_timer.start_timer;
     OPEN employee_cur;
     LOOP
@@ -27,7 +54,7 @@ CREATE OR REPLACE PACKAGE BODY bas_col IS
     END LOOP;
     CLOSE employee_cur;
     sf_timer.show_elapsed_time('-------End block records---------');
-     -- WORK WHITH ASSOCIATIVE COLLECTIONS
+    -- WORK WHITH ASSOCIATIVE COLLECTIONS
     sf_timer.start_timer;
     OPEN employee_cur;
     LOOP
@@ -39,7 +66,7 @@ CREATE OR REPLACE PACKAGE BODY bas_col IS
     END LOOP;
     CLOSE employee_cur;
     sf_timer.show_elapsed_time('-------End block collections---------');
-      -- WORK WHITH BULK COLLECTIONS
+    -- WORK WHITH BULK COLLECTIONS
     sf_timer.start_timer;
     OPEN employee_cur;
     LOOP
@@ -54,3 +81,22 @@ CREATE OR REPLACE PACKAGE BODY bas_col IS
     sf_timer.show_elapsed_time('-------End block bulk collect---------');
   END;
 END bas_col;
+
+/
+BEGIN
+  -- test procedure bas_col.output_employees;
+  BEGIN
+    bas_col.output_employees;
+  END;
+  -- test procedure bas_col.change_park
+  DECLARE
+    park_rec auto_park%ROWTYPE;
+  BEGIN
+    change_park('AUTOPARK', park_rec);
+    COMMIT;
+    dbms_output.put_line(park_rec.title);
+    FOR i IN park_rec.names_auto.first .. park_rec.names_auto.last LOOP
+      dbms_output.put_line(park_rec.names_auto(i));
+    END LOOP;
+  END; 
+END;
