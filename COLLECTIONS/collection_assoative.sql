0299fbc90732869a55a865761ff229624d14d3c8
laravel.dev <basket.baza@gmail.com>
2022-03-08 23:14:03 +0300

    COLLECTION ASSOATIVE

--- COLLECTIONS/collection_assoative.sql
+++ COLLECTIONS/collection_assoative.sql
@@ -6,6 +6,7 @@ BEGIN
   population('BELARUS') := 9500000;
   population('RUSSIA') := 140500000;
   population('UKRAIN') := 35000000;
+  population('RUSSIA') := 135500000;
   i := population.first;
   WHILE i IS NOT NULL LOOP
     dbms_output.put_line('Population in ' || i || ' = ' || population(i));
