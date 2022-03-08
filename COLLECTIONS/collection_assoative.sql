5a7afe9ac0070501cd2db85866d24705947ecaf6
laravel.dev <basket.baza@gmail.com>
2022-03-08 23:12:40 +0300

    COLLECTION ASSOATIVE

---
+++ COLLECTIONS/collection_assoative.sql
@@ -0,0 +1,15 @@
+﻿DECLARE
+  TYPE population_aat IS TABLE OF PLS_INTEGER INDEX BY VARCHAR2(20);
+  population population_aat;
+  i          VARCHAR2(20);
+BEGIN
+  population('BELARUS') := 9500000;
+  population('RUSSIA') := 140500000;
+  population('UKRAIN') := 35000000;
+  i := population.first;
+  WHILE i IS NOT NULL LOOP
+    dbms_output.put_line('Population in ' || i || ' = ' || population(i));
+    i := population.next(i);
+  END LOOP;
+
+END;
