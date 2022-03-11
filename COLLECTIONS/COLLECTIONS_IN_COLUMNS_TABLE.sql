2edeeac2cfc6faeb7378c9d4ddeb73a374dcc2f4
laravel.dev <basket.baza@gmail.com>
2022-03-12 01:36:09 +0300

    COLLECTIONS_IN_COLUMNS_TABLE.sql

---
+++ COLLECTIONS/COLLECTIONS_IN_COLUMNS_TABLE.sql
@@ -0,0 +1,40 @@
+﻿-- ОБЯЪЯВЛЯЕМ ГЛОБАЛЬНЫЕ ТИПЫ
+CREATE OR REPLACE TYPE fio_list_ot AS OBJECT
+(
+  fio VARCHAR2(500)
+)
+
+CREATE OR REPLACE TYPE fio_ntt IS TABLE OF VARCHAR2(500);--emp_list_ot;
+
+CREATE OR REPLACE TYPE fio_vat IS VARRAY(20) OF VARCHAR2(500);--emp_list_ot;
+
+DROP TABLE tab_with_collect;
+
+-- СОЗДАЕМ ТАБЛИЦУ С ДВУМЯ ВИДАМИ СТОЛБЦОВ ТИПА КОЛЛЕКЦИИ
+CREATE TABLE tab_with_collect(ID NUMBER CONSTRAINT tab_with_collect_pk PRIMARY KEY, job_id VARCHAR2(20) CONSTRAINT tab_with_collect_fk REFERENCES jobs, fio_nt fio_ntt, fio_at fio_vat) NESTED TABLE fio_nt store AS tab_with_fio;
+
+--ВСТАВЛЯЕМ ДАННЫЕ
+ INSERT INTO tab_with_collect
+  (id, job_id, fio_nt, fio_at)
+VALUES
+  (1, 'FI_MGR', fio_ntt('nt_tek'), fio_vat('at_tek'));
+----
+ INSERT INTO tab_with_collect
+  (id, job_id, fio_nt, fio_at)
+VALUES
+  (2, 'FI_MGR', fio_ntt('nt_bas'), fio_vat('at_bas'));
+----
+ INSERT INTO tab_with_collect
+  (id, job_id, fio_nt, fio_at)
+VALUES
+  (3, 'FI_MGR', fio_ntt('nt_bas', 'nt_tek'), fio_vat('at_bas', 'at_tek'));
+--- 
+--Выборка данных
+SELECT id,
+       job_id,
+       (SELECT listagg(COLUMN_VALUE, ', ') WITHIN GROUP(ORDER BY 1)
+          FROM TABLE(t.FIO_NT)) AS FIO_NT,
+       (SELECT listagg(COLUMN_VALUE, ', ') WITHIN GROUP(ORDER BY 1)
+          FROM TABLE(t.FIO_AT)) AS FIO_AT
+
+  FROM (SELECT * FROM tab_with_collect) t
--- COLLECTIONS/collection_assoative.sql
+++ COLLECTIONS/collection_assoative.sql
@@ -1,11 +1,13 @@
 ﻿DECLARE
   TYPE population_aat IS TABLE OF PLS_INTEGER INDEX BY VARCHAR2(20);
-  population population_aat;
+  population  population_aat := population_aat('BELARUS' => 9500000,
+                                              'RUSSIA'  => 140500000,
+                                              'UKRAIN'  => 35000000);
   i          VARCHAR2(20);
 BEGIN
-  population('BELARUS') := 9500000;
-  population('RUSSIA') := 140500000;
-  population('UKRAIN') := 35000000;
+  --population('BELARUS') := 9500000;
+  --population('RUSSIA') := 140500000;
+  --population('UKRAIN') := 35000000;
   population('RUSSIA') := 135500000;
   i := population.first;
   WHILE i IS NOT NULL LOOP
